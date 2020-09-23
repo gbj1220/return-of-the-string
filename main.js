faa9d6b7f419c366f5b96ba43d5b3f84f09a1dee
Gregory.Johnson <3531op@gmail.com>
2020-09-23 10:00:29 -0400

    Completed main.js

--- main.js
+++ main.js
@@ -2,10 +2,48 @@
  * YOUR CODE HERE! *
  *******************/
 
+function yell(name) {
+  name = name + '!'
+  rndName = name;
+  return rndName; 
+}
+
+function getFirstCharacter(char) {
+  return char[0]
+}
+  
+function getLastCharacter(char) {
+  return char[char.length-1]
+}
 
 
+function getOneCharacter(str, index) {
+  return str [index]
+}
+
+let number = getOneCharacter('Greg',0)
+number;
 
 
+function getTwoCharacters(str, num1, num2) {
+  return str[num1] + str[num2] 
+
+}
+
+function makeCapitalized(str) {
+  return str.toUpperCase('greg'); 
+  
+}
+
+function yellLouder(threeExCap) {
+  return threeExCap.toUpperCase() + '!!!'
+}
+
+function getInitials(str) {
+  return str[0] + '.' + [0]
+}
+
+  
 
 /********************************************************************
  * THIS CODE IS FOR INTERNAL USE ONLY. DON'T CHANGE ANYTHING BELOW! *
